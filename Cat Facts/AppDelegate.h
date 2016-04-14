8c3a8baeb4e048f310d20d65462ea3e9f6a6b00d
Danny Murphy <Dmurph24@gmail.com>
2016-04-14 16:59:39 -0400

    Initial Commit

---
+++ Cat Facts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cat Facts
+//
+//  Created by Daniel Murphy on 4/14/16.
+//  Copyright © 2016 Daniel Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cat Facts/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Cat Facts
+//
+//  Created by Daniel Murphy on 4/14/16.
+//  Copyright © 2016 Daniel Murphy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
